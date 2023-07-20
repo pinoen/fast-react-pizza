7272c748590f1403496c788aa03c53c18d60b159
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-20 18:15:02 -0300

    add funtionality to delete btn dispatching the proper action

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,11 @@
+import { useDispatch } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import { deleteItem } from "./cartSlice";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+  const dispatch = useDispatch()
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -11,7 +14,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type='small'>Delete</Button>
+        <Button onClick={() => dispatch(deleteItem(pizzaId))} type='small'>Delete</Button>
       </div>
     </li>
   );
