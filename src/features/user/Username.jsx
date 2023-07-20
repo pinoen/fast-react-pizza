d8ce474e7bcdc0a8da8d2f00dbe7c9900c3e9823
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-20 12:35:17 -0300

    install redux toolkit and create store file the handle user global state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,8 +1,12 @@
+import { useSelector } from "react-redux"
 
 
 const Username = () => {
+  const username = useSelector(store => store.user.username)
+  if (!username) return null;
+
   return (
-    <div className="text-sm font-semibold hidden md:block">Emilio</div>
+    <div className="text-sm font-semibold hidden md:block">{username}</div>
   )
 }
 
