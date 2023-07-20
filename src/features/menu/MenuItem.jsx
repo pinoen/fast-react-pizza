b4a7bc8ee86a75faf8eeef7e87e0d518229e4c1a
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-20 18:16:25 -0300

    Add delete btn and then render it conditionally along with the order btn, depending on if was o was not selected

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
-import { addItem } from '../cart/cartSlice'
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice'
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch()
+  const currentQuantity = useSelector(getCurrentQuantityById(id))
+  const isInCart = currentQuantity > 0
 
   function handleAddToCart() {
     const newItem = {
@@ -25,9 +27,13 @@ function MenuItem({ pizza }) {
       <div className="flex flex-col grow mt-0.5">
         <p className="font-medium">{name}</p>
         <p className="capitalize text-sm italic text-stone-500">{ingredients.join(', ')}</p>
+
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
-          {!soldOut && <Button onClick={handleAddToCart} type='small'>Add to cart</Button>}
+
+          {isInCart > 0 && <Button type='small'>Delete</Button>}
+
+          {!soldOut && !isInCart && <Button onClick={handleAddToCart} type='small'>Add to cart</Button>}
         </div>
       </div>
     </li>
