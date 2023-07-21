394ee7b9a184a9f1e7a4ed2db7f92db68cc26977
Emilio Pino <pinoen@yahoo.com.ar>
2023-07-20 22:31:28 -0300

    use of currency format function along with the totalcartprice

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity)
@@ -12,7 +13,7 @@ function CartOverview() {
     <div className="flex items-center justify-between bg-stone-800 text-stone-200 uppercase p-4 sm:p-6 text-sm sm:text-base">
       <p className="text-stone-300 font-semibold space-x-4 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>${totalCartPrice}</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to={'/cart'}>Open cart &rarr;</Link>
     </div>
